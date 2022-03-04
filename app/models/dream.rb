7fff2493a279b5eb201af3836ef805a52d0a2f64
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2022-03-04 18:33:15 +0000

    validation dream corrected

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -3,6 +3,6 @@ class Dream < ApplicationRecord
   belongs_to :significance
   has_and_belongs_to_many :labels
 
-  validates :content, presence: true, length: { min: 5 }
-  validates :dream_date, comparison: { lower_than: :created_at}
+  validates :content, presence: true, length: { mininum: 5 }
+  # validates :dream_date, comparison: { lower_than: :created_at }
 end
