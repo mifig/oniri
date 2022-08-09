dae7cd3a97e7418bbdccf81634f86c55a04b5c5f
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2022-08-09 14:59:58 +0100

    transformed into PWA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "9fff-2001-818-d825-5a00-1d3d-3b38-c39a-d2fd.eu.ngrok.io"
+  config.hosts << "12e5-2001-818-d825-5a00-dd95-fdc5-d1e8-969d.eu.ngrok.io"
 end
